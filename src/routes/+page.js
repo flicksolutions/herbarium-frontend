0965d4ab53eaba258cc091040ff1c4b9f785e7d7
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2024-07-10 17:38:17 +0200

    sort items initially for genus and then species

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -6,6 +6,12 @@ export async function load() {
 	return {
 		categories: Object.keys(itemData[0]),
 		itemstructure: structure,
-		items: itemData
+		items: itemData.sort((a, b) => {
+			if (a.genus < b.genus) return -1;
+			if (a.genus > b.genus) return 1;
+			if (a.specificEpithet < b.specificEpithet) return -1;
+			if (a.specificEpithet > b.specificEpithet) return 1;
+			return 0;
+		})
 	};
 }
