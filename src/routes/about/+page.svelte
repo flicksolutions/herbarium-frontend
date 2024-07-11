840cd44a096f83ca6571d7a24e33f88af39e60b8
Sebi <sebastian@flick.solutions>
2024-07-11 17:42:51 +0200

    add about content

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,16 +4,112 @@
 
 <ContentContainer>
 	<h1 class="h1">About the project</h1>
-	<p>some content here.</p>
 	<p>
-		<a
-			class="anchor"
-			target="_blank"
-			href="https://www.dh.unibe.ch/ueber_uns/personen/flick_sebastian/index_ger.html"
-		>
-			Sebastian Flick
-		</a>
-		(<a class="anchor" target="_blank" href="https://www.dsl.unibe.ch/">Data Science Lab</a>,
-		University of Bern).
+		The Herbarium of the Botanical Garden of the University of Bern (Herbarium Bernense) is one of
+		the five largest herbaria in Switzerland. This collection includes plants, algae and fungi
+		either dried or preserved in alcohol. The specimens can last for centuries and represent a
+		unique long-term archive of the occurrence, distribution, and diversity of the respective
+		organisms.
 	</p>
+	<h2 class="h2">Collections of the Herbarium Bernense</h2>
+	<ul class="list-disc list-inside">
+		<li>
+			Herbarium specimens: The largest collection of the Herbarium Bernense are the herbarium
+			specimens, pressed and mounted on herbarium sheets. The oldest of the estimated 500,000
+			specimens dates back to the year 1753.
+		</li>
+		<li>
+			Wet collection: The wet collection, preserved in an 75% ethanol solution, is probably the
+			largest historical wet collection of plants and fungi in Switzerland and comprises 1,142
+			specimens.
+		</li>
+		<li>
+			Dry bulk collection: This collection comprises about 3000 specimens of seeds, woody fruits,
+			tree cross sections and fungi fruit bodies.
+		</li>
+		<li>
+			Fossil collection: About 3000 specimens of fossilized plants and archaeological finds (e.g.
+			fossilised grains from prehistoric pile dwellings) are contained in this collection.
+		</li>
+		<li>
+			Botanical illustrations: Botanical illustrations have the advantage that they can show natural
+			colours better than preserved specimens, such as the magnificent water colours of fungi by
+			<a href="http://www.botany.unibe.ch/gallery/otth.php">Gustav Otth</a> (1806-1874).
+		</li>
+		<li>
+			Photo collection: Besides slides and photographs, the Herbarium Bernense includes several
+			artistically hand-coloured glass plates, including those by Josef Hanel (1865-1940). He
+			enjoyed a high reputation as a master of the photographic craft and as a gifted artist.
+		</li>
+	</ul>
+	<h2 class="h2">Reconditioning and digitization</h2>
+	<p>
+		Since 2022, four projects are currently running to recondition and digitize the first 10% of the
+		Herbarium Bernense collection. These projects were made possible by the Swiss Network of Natural
+		History Collections (<a href="https://swisscollnet.scnat.ch/de">SwissCollNet</a>):
+	</p>
+	<ul class="list-disc list-inside">
+		<li>
+			"Efficient digitisation of herbarium specimens through the use of digitisation routes". In
+			collaboration with the <a href="http://www.naturama.ch/museum">Naturama Aargau</a>
+		</li>
+		<li>
+			"Historical Swiss alcohol collections of plants and fungi: rare and hidden treasures". In
+			collaboration with the <a href="https://www.botmuseum.uzh.ch/de.html"
+				>Botanical Museum of the University of Zurich</a
+			>
+		</li>
+		<li>
+			"Digitisation of the Fribourg (NHMF) and Bern (BERN) herbaria". In collaboration with the
+			<a href="https://www.fr.ch/de/nhmf">Natural History Museum of Fribourg</a> and the Botanical
+			Garden of the University of Fribourg,
+			<a href="https://www.unifr.ch/jardin-botanique/de/"
+				>Botanical Garden of the University of Fribourg</a
+			>
+		</li>
+		<li>
+			"Digitisation of the three-dimensional dry collection and associated herbarium specimens" In
+			collaboration with the <a href="https://www.botmuseum.uzh.ch/de.html"
+				>Botanical Museum of the University of Zurich</a
+			>, the
+			<a href="https://www.herbarien.uzh.ch/de.html">Herbaria of the University and ETH Zurich</a>
+			and the
+			<a href="https://stadt.winterthur.ch/naturmuseum">Naturmuseum Winterthur</a>
+		</li>
+	</ul>
+	<h2 class="h2">Funding</h2>
+	<p>
+		Reconditioning and digitization of the Herbarium Bernense are kindly funded by the following
+		institutions:
+	</p>
+	<div class="logo-cloud grid-cols-1 lg:!grid-cols-3 gap-1">
+		<a class="logo-item" href="/">
+			<span>(icon)</span>
+			<span>Skeleton</span>
+		</a>
+		<a class="logo-item" href="/">
+			<span>(icon)</span>
+			<span>Skeleton</span>
+		</a>
+		<a class="logo-item" href="/">
+			<span>(icon)</span>
+			<span>Skeleton</span>
+		</a>
+		<a class="logo-item" href="/">
+			<span>(icon)</span>
+			<span>Skeleton</span>
+		</a>
+		<a class="logo-item" href="/">
+			<span>(icon)</span>
+			<span>Skeleton</span>
+		</a>
+		<a class="logo-item" href="/">
+			<span>(icon)</span>
+			<span>Skeleton</span>
+		</a>
+		<a class="logo-item" href="/">
+			<span>(icon)</span>
+			<span>Skeleton</span>
+		</a>
+	</div>
 </ContentContainer>
