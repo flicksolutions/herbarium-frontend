edf589bc7b00f0311cabce27b04b5876ff7515f9
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2024-07-18 11:42:06 +0200

    add impressum

--- src/routes/impressum/+page.svelte
+++ src/routes/impressum/+page.svelte
@@ -4,5 +4,47 @@
 
 <ContentContainer>
 	<h1 class="h1">Impressum</h1>
-	<p>Who is responsible?</p>
+	<div class="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-4">
+		<div>
+			<h2 class="h2">Location</h2>
+			<p>
+				Herbarium des Botanischen Gartens Bern<br />
+				Länggassstrasse 31-37<br />
+				3012 Bern<br />
+				Switzerland
+			</p>
+		</div>
+		<div>
+			<h2 class="h2">Postal Address</h2>
+			<p>
+				Unitobler<br />
+				Herbarium des Botanischen Gartens Bern<br />
+				Länggassstrasse 49<br />
+				3012 Bern
+			</p>
+		</div>
+		<div>
+			<h2 class="h2">Contact</h2>
+			<p>
+				Dr. Katja Rembold (curator)<br />
+				<a class="anchor" href="mailto:katja.rembold@unibe.ch">katja.rembold@unibe.ch</a>
+			</p>
+			<p>
+				Rahel Vetsch (project coordinator)<br />
+				<a class="anchor" href="mailto:rahel.vetsch@unibe.ch">rahel.vetsch@unibe.ch</a>
+			</p>
+			<p>
+				Phone: <a class="anchor" href="tel:+41316844973">+41 31 684 49 73</a>
+			</p>
+		</div>
+		<div>
+			<h2 class="h2">Index Herbariorum</h2>
+			<p>
+				Herbarium Code: <a
+					class="anchor"
+					href="https://sweetgum.nybg.org/science/ih/herbarium-details/?irn=126603">BERN</a
+				>
+			</p>
+		</div>
+	</div>
 </ContentContainer>
