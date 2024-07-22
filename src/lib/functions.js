a67eb1bb51e47b5a1a62cc2cde83d3e42ed6148e
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2024-07-22 14:37:48 +0200

    make setGenusAndSpeciesItalic safe from ???

--- src/lib/functions.js
+++ src/lib/functions.js
@@ -10,7 +10,10 @@ export const setGenusAndSpeciesItalic = (
 	/** @type {string} */ genus,
 	/** @type {string} */ species
 ) => {
-	return input.replace(new RegExp(`${genus}|${species}`, 'g'), `<span class="italic">$&</span>`);
+	return input.replace(
+		new RegExp(`${genus.replaceAll('?', '')}|${species.replaceAll('?', '')}`, 'g'),
+		`<span class="italic">$&</span>`
+	);
 };
 
 export const openLightbox = (/** @type {any} */ target) => {
